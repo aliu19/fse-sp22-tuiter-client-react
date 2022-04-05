45e9ee3f32fb4bd5af303b0e6e22a162710a0add
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-04 23:23:25 -0400

    added jsdoc for test

--- src/tests/edit-profile-screen.test.js
+++ src/tests/edit-profile-screen.test.js
@@ -1,3 +1,6 @@
+/**
+ * @file Implements tests for edit-profile screen
+ */
 import React from "react";
 import EditProfile from "../components/profile/edit-profile";
 import alice from "../components/profile/alice-data.json"
@@ -5,6 +8,10 @@ import {api, profile} from "../services/auth-service";
 import {render, screen, waitFor} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 
+/**
+ * Test that if edit-profile component can render user's profile information correctly
+ * with mocked profile
+ */
 describe("renders user's profile information mocked correctly", () => {
     const mock = jest.spyOn(api, 'post');
     const mockedProfile = alice;
