56b525fd3fe0ca458262e3baec8453c2e16a45e0
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-04 22:58:45 -0400

    export axios for testing purpose

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -7,7 +7,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const AUTH_API = `${BASE_URL}/api/auth`
 
-const api = axios.create({
+export const api = axios.create({
     withCredentials: true
 });
 
