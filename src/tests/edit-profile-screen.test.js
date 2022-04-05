1e4f04bb3578e3cfca4c802a1e1721b6d81b75eb
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-04 23:21:44 -0400

    modify the static alice user data and finsihed test

---
+++ src/tests/edit-profile-screen.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import EditProfile from "../components/profile/edit-profile";
+import alice from "../components/profile/alice-data.json"
+import {api, profile} from "../services/auth-service";
+import {render, screen, waitFor} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+
+describe("renders user's profile information mocked correctly", () => {
+    const mock = jest.spyOn(api, 'post');
+    const mockedProfile = alice;
+    afterAll(() => {
+        mock.mockRestore()
+    })
+
+    test("renders user's profile information mocked correctly", async () => {
+        mock.mockImplementation(() => {
+            return Promise.resolve({data: mockedProfile});
+        })
+
+        render(
+            <HashRouter>
+                <EditProfile/>
+            </HashRouter>
+        )
+
+        await waitFor(() => {
+            for (const key in mockedProfile) {
+                if (key === "_id") {
+                    return;
+                }
+                const profileAttribute = mockedProfile[key];
+                const profileElement = screen.getByDisplayValue(profileAttribute);
+                expect(profileElement).toBeInTheDocument();
+            }
+        })
+    })
+})
