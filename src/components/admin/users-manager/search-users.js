f477d4527e7fc5f8f4db59e5e0cf9992ec604909
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-12 19:40:06 -0400

    worked on search user page

--- src/components/admin/users-manager/search-users.js
+++ src/components/admin/users-manager/search-users.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SearchUsers = () => {
+    const [searchName, setSearchName] = useState("");
     return (
         <div>
             <h2>Search users</h2>
+            <div className="row">
+                <div className="col-9">
+                    <input value={searchName}
+                           onChange={(event) => {
+                               setSearchName(event.target.value)
+                           }}
+                           className="form-control"
+                           placeholder="please search User's username"/>
+                </div>
+                <div className="col-3">
+                    <button
+                        className="btn btn-primary btn-block">
+                        Search
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
