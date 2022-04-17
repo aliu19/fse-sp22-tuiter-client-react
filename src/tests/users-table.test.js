f76a4813b61194b2dd05b1a521307e6feb8b524a
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-16 22:23:48 -0400

    added test calling async api

--- src/tests/users-table.test.js
+++ src/tests/users-table.test.js
@@ -1,8 +1,9 @@
 import UsersTable from "../components/admin/users-manager/users-table";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
-import {findAllUsers} from "../services/users-service";
+import {createUser, deleteUsersByUsername, findAllUsers} from "../services/users-service";
 import axios from "axios";
+import {UserList} from "../components/profile/user-list";
 
 const MOCKED_USERS = [
     {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
@@ -18,6 +19,35 @@ test("User table renders static user array", () => {
     )
     MOCKED_USERS.forEach(u => {
         const userElement = screen.getByText(u.username, {exact: false})
+        expect(userElement).toBeInTheDocument();
+    })
+})
+
+describe('user table renders async', () => {
+    // sample user to insert
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com',
+        role: 'ADMIN'
+    };
+
+    beforeAll(() => {
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    afterAll(() => {
+        return deleteUsersByUsername(ripley.username);
+    })
+
+    test('user table renders async', async () => {
+        const newUser = await createUser(ripley);
+        const users = await findAllUsers();
+        render(
+            <HashRouter>
+                <UserList users={users}/>
+            </HashRouter>);
+        const userElement = screen.getByText(newUser.username);
         expect(userElement).toBeInTheDocument();
     })
 })
