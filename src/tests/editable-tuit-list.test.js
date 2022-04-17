3defa0345a2f832876e2fe3c61e812fc86af5f97
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-16 22:29:39 -0400

    Added test for editable tuit list calling async api

--- src/tests/editable-tuit-list.test.js
+++ src/tests/editable-tuit-list.test.js
@@ -1,7 +1,8 @@
 import EditableTuits from "../components/admin/tuits-manager/editable-tuits";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
-import {api, findAllTuits} from "../services/tuits-service";
+import {api, createTuitByUser, deleteTuitByContent, findAllTuits} from "../services/tuits-service";
+import {createUser, deleteUsersByUsername} from "../services/users-service";
 
 const MOCKED_TUITS = [
     {
@@ -56,7 +57,74 @@ test('editable tuit list renders static tuit array'
     expect(editButtons.length).toBe(3);
 });
 
-test('tuit list renders mocked', async () => {
+describe('editable tuit list renders async'
+         + 'with edit-button', ()=> {
+    // samples tuits we'll insert to then retrieve
+    const tuitContents = [
+        "adam's Tuit 1", "adam's Tuit 2", "adam's Tuit 3"
+    ]
+
+    // sample user to insert as the author of tuits
+    const user = {
+        username: 'adam_smith',
+        password: 'adam123',
+        email: 'wealth@nations.com'
+    };
+
+    // setup data before test
+    beforeAll(() => {
+        let promises = [];
+        // delete the user we inserted
+        promises.push(deleteUsersByUsername(user.username));
+        // delete tuits we inserted
+        tuitContents.map(content => {
+            promises.push(deleteTuitByContent(content));
+        })
+        return Promise.all(promises);
+    })
+
+    // clean up after test runs
+    afterAll(()=> {
+        let promises = []
+        // delete the user we inserted
+        promises.push(deleteUsersByUsername(user.username));
+        // delete tuits we inserted
+        tuitContents.map((content) => {
+            let deletePromise = deleteTuitByContent(content);
+            promises.push(deletePromise)
+        })
+        return Promise.all(promises)
+    })
+
+    test('editable tuit list renders async'
+         + 'with edit-button', async () => {
+        const author = await createUser(user);
+        let promises = [];
+        tuitContents.map(content => {
+            promises.push(createTuitByUser(author._id, {tuit: content, postedOn: "2022-03-09T00:00:00.000Z"}));
+        })
+        await Promise.all(promises);
+        const tuits = await findAllTuits();
+        render(
+            <HashRouter>
+                <EditableTuits allTuits={tuits}/>
+            </HashRouter>
+        )
+
+        // Knowing there are 3 tuits by adam
+        tuitContents.forEach(content => {
+            const tuitElements = screen.getAllByText(content, {exact: false});
+            tuitElements.forEach(e => expect(e).toBeInTheDocument());
+        })
+        // knowing there is adman names
+        let nameElements = screen.getAllByText(user.username, {exact: false});
+        nameElements.forEach((e) => expect(e).toBeInTheDocument());
+        const editButtons = screen.getAllByTestId('edit-button', {exact: true});
+        expect(editButtons.length).toBeGreaterThanOrEqual(3);
+    })
+})
+
+test('editable tuit list renders mocked', async () => {
     const mock = jest.spyOn(api, 'get');
     mock.mockImplementation(() =>
                                 Promise.resolve({data: {tuits: MOCKED_TUITS}}));
