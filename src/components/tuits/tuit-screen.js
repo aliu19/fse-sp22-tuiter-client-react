243745dbe6a203818fd0cdfa30bfb9e7382dbec0
averywpx <averywpx@gmail.com>
2022-04-26 19:25:22 -0400

    working on general update

--- src/components/tuits/tuit-screen.js
+++ src/components/tuits/tuit-screen.js
@@ -2,15 +2,68 @@ import {useEffect, useState} from "react";
 import * as service from "../../services/tuits-service"
 import {useParams} from "react-router-dom";
 import Tuit from "./tuit";
+import * as likeService from "../../services/likes-service";
+import * as dislikeService from "../../services/dislikes-service"
+import * as authService from "../../services/auth-service";
+
 
 const TuitScreen = () => {
     const [tuit, setTuit] = useState({});
+    const [editing, setEditing] = useState(false);
     const {tid} = useParams();
+
+    const [profile, setProfile] = useState(undefined);
+    useEffect(async ()=> {
+        try {
+            const user = await authService.profile();
+            if (user) {
+                setProfile(user);
+            }
+        } catch (e) {
+        }
+    }, []);
+
     const findTuitById = () =>
         service.findTuitById(tid)
             .then(tuit => {
                 setTuit(tuit)
             });
+
+    const updateTuit = (newTuit) => {
+        console.log('update tuit', newTuit)
+        service.updateTuit(newTuit._id,newTuit)
+        }
+
+    /**
+     * Callback function to fetch API to toggle likes of a tuit
+     * when user clicks like button
+     * @param tuit Tuit that was liked
+     */
+     const likeTuit = (tuit) => {
+        if (profile !== undefined) {
+            likeService.userTogglesTuitLikes("me", tuit._id)
+                .then(findTuitById())
+                .catch(e => alert(e));
+        } else {
+            alert("Please log in!")
+        }
+    }
+
+    /**
+     * Callback function to fetch API to toggle dislikes of a tuit
+     * when user clicks dislike button
+     * @param tuit Tuit that was disliked
+     */
+    const dislikeTuit = (tuit) => {
+        if (profile !== undefined) {
+            dislikeService.userTogglesTuitDislikes("me", tuit._id)
+                .then(findTuitById())
+                .catch(e => alert(e));
+        } else {
+            alert("Please log in!")
+        }
+    }
+
     useEffect(() => {
         findTuitById();
     }, []);
@@ -19,11 +72,29 @@ const TuitScreen = () => {
         <div>
             <div className="mb-3">
             {console.log('tuit-screen',tuit)}
-            <Tuit tuit={tuit} likeTuit={() => {}}/>
+            <Tuit 
+            likeTuit={likeTuit}
+            dislikeTuit={dislikeTuit}
+            tuit={tuit}/>
         </div>
-            <button type="button" className="btn btn-primary me-2">
+            {tuit.ownedByMe && !editing &&
+                <button type="button" 
+                        className="btn btn-primary me-2"
+                        onClick={() => {
+                            setEditing(true)
+                        }}>
                 Update
             </button>
+            }
+            {
+                editing &&
+                <button type="button" 
+                        className="btn btn-success me-2"
+                        onClick={() => {
+                            setEditing(false)
+                            updateTuit(tuitCache) 
+                        }}></button>
+            }
         </div>
     );
 };
