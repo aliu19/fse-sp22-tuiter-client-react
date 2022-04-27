272b0c70fdf3f685d07bbd1271be60e2b1c61571
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-26 22:15:01 -0400

    Added alert message for sign up, login. When sign up default user is general user

--- src/components/profile/signup.js
+++ src/components/profile/signup.js
@@ -10,15 +10,25 @@ import {useNavigate} from "react-router-dom";
  */
 
 const Signup = () => {
-    const [newUser, setNewUser] = useState({});
+    const [newUser, setNewUser] = useState({role: 'GENERAL'});
     const navigate = useNavigate();
     // const [userName, setUsername] = useState("");
     // const [userRole, setUserRole] = useState("");
 
-    const signup = () =>
+    const signup = () => {
+        if (newUser.username === '' || newUser.username === undefined
+            || newUser.password === '' || newUser.password === undefined
+            || newUser.email === '' || newUser.email === undefined) {
+            alert("Username, password and email cannot be empty!")
+            return
+        }
         service.signup(newUser)
-            .then(() => navigate('/home'))
-            .catch(e => alert(e));
+            .then(() => {
+                alert('Account is successfully created!')
+                navigate('/profile')
+            })
+            .catch(e => alert("Username is already taken, please try different username!"));
+    }
 
     return (
         <div>
@@ -38,12 +48,14 @@ const Signup = () => {
             <div className="form-group row">
                     <label className="col-sm-4 col-form-label">Sign up as:</label>
                     <div className="col-sm-4 form-check">
-                        <input className="form-check-input" 
+                        <input className="form-check-input"
                             name='role'
                             type="radio"
                             value='GENERAL'
-                            onChange={(e) =>
-                                setNewUser({...newUser, role: e.target.value})}/>
+                               checked={newUser.role === 'GENERAL'}
+                            onChange={(e) => {
+                                setNewUser({...newUser, role: e.target.value})
+                            }}/>
                         <label className="form-check-label">
                             GENERAL
                         </label>
@@ -53,6 +65,7 @@ const Signup = () => {
                             name='role'
                             type="radio"
                             value='ADMIN'
+                               checked={newUser.role === 'ADMIN'}
                             onChange={(e) =>
                                 setNewUser({...newUser, role: e.target.value})}/>
                         <label className="form-check-label">
